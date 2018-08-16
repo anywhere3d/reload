4c0fb1cdc760dfb8d3e38e74a6e369adb5da5ed8
anywhere3D <anywhere3d@gmail.com>
2018-08-16 20:59:28 +0200

    Add files via upload

---
+++ models/cameraFrontPlane.js
@@ -0,0 +1,35 @@
+{
+    "faces": [34,1,0,4,0,0,0,0,34,3,1,4,0,0,0,0,34,0,2,4,0,0,0,0,34,2,3,4,0,0,0,0],
+    "metadata": {
+        "faces": 4,
+        "version": 3,
+        "uvs": 0,
+        "generator": "io_three",
+        "vertices": 5,
+        "materials": 1,
+        "type": "Geometry",
+        "normals": 1
+    },
+    "vertices": [-1,0.75,-2,1,0.75,-2,-1,-0.75,-2,1,-0.75,-2,0,-0,-2],
+    "uvs": [],
+    "materials": [{
+        "opacity": 1,
+        "DbgName": "Material",
+        "DbgIndex": 0,
+        "blending": "NormalBlending",
+        "colorEmissive": [0,0,0],
+        "shading": "phong",
+        "specularCoef": 50,
+        "visible": true,
+        "DbgColor": 15658734,
+        "depthTest": true,
+        "colorSpecular": [0.5,0.5,0.5],
+        "colorDiffuse": [0.64,0.64,0.64],
+        "wireframe": false,
+        "colorAmbient": [0.64,0.64,0.64],
+        "depthWrite": true,
+        "transparent": false
+    }],
+    "name": "PlaneGeometry.1",
+    "normals": [0,0,1]
+}
